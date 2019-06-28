cebb6b13b95c6f41b51857ea917d417b7c098bfb
chaofengwu <wuchaofeng47@gmail.com>
2019-06-28 18:41:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email="fmfnogueira@gmail.com",
     description='Bayesian Optimization package',
     long_description='A Python implementation of global optimization with gaussian processes.',
-    # download_url='https://github.com/fmfn/BayesianOptimization/tarball/0.6',
+    download_url='https://github.com/fmfn/BayesianOptimization/tarball/0.6',
     install_requires=[
         "numpy >= 1.9.0",
         "scipy >= 0.14.0",
