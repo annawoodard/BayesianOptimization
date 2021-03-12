24362cc65369de2b2e94a80e2dc6efaf3ef49ebe
Anna Elizabeth Woodard <awoodard@nd.edu>
2021-03-12 14:57:49 -0600

    Add hacked version of constrained optimizer

--- bayes_opt/constrained_bayesian.py
+++ bayes_opt/constrained_bayesian.py
@@ -5,7 +5,7 @@ from .event import Events, DEFAULT_EVENTS
 from .logger import _get_default_logger
 from .util import UtilityFunction, acq_max, ensure_rng
 
-from sklearn.gaussian_process.kernels import Matern
+from sklearn.gaussian_process.kernels import Matern, WhiteKernel
 from sklearn.gaussian_process import GaussianProcessRegressor
 
 
@@ -41,6 +41,7 @@ class Observable(object):
     Inspired/Taken from
         https://www.protechtraining.com/blog/post/879#simple-observer
     """
+
     def __init__(self, events):
         # maps event names to subscribers
         # str -> dict
@@ -51,7 +52,7 @@ class Observable(object):
 
     def subscribe(self, event, subscriber, callback=None):
         if callback is None:
-            callback = getattr(subscriber, 'update')
+            callback = getattr(subscriber, "update")
         self.get_subscribers(event)[subscriber] = callback
 
     def unsubscribe(self, event, subscriber):
@@ -62,7 +63,9 @@ class Observable(object):
             callback(event, self)
 
 
-class BayesianOptimization(Observable):
+
+from .utility import UtilityFunction, acq_max, ensure_rng
+class ConstrainedBayesianOptimization(Observable):
     """
     This class takes the function to optimize as well as the parameters bounds
     in order to find which values for the parameters yield the maximum value
@@ -101,33 +104,46 @@ class BayesianOptimization(Observable):
     set_bounds()
         Allows changing the lower and upper searching bounds
     """
-    def __init__(self, f, pbounds, alpha=1e-10, random_state=None, verbose=2,
-                 bounds_transformer=None):
-        self._random_state = ensure_rng(random_state)
 
-        # Data structure containing the function to be optimized, the bounds of
-        # its domain, and a record of the evaluations we have done so far
-        self._space = TargetSpace(f, pbounds, random_state)
+    def __init__(
+        self, f, pbounds, random_state=None, verbose=2, bounds_transformer=None, tags=None
+    ):
+        if tags is None:
+            self._tags = ['objective', 'constraint']
+
+        self._random_state = ensure_rng(random_state)
 
         self._queue = Queue()
 
-        # Internal GP regressor
-        self._gp = GaussianProcessRegressor(
-            kernel=Matern(nu=2.5),
-            alpha=alpha,
-            normalize_y=True,
-            n_restarts_optimizer=5,
-            random_state=self._random_state,
-        )
-
         self._verbose = verbose
         self._bounds_transformer = bounds_transformer
-        if self._bounds_transformer:
+
+        self._space = {}
+        self._gp = {}
+        self._bounds_transformer = {}
+        for tag in self._tags:
+            # Data structure containing the function to be optimized, the bounds of
+            # its domain, and a record of the evaluations we have done so far
+            self._space[tag] = TargetSpace(f, pbounds, random_state)
+
+
+            # Internal GP regressor
+            self._gp[tag] = GaussianProcessRegressor(
+                kernel=Matern(nu=2.5) + WhiteKernel(noise_level=1e-3),
+                alpha=1e1,
+                normalize_y=True,
+                n_restarts_optimizer=5,
+                random_state=self._random_state,
+            )
+
+        if bounds_transformer:
+            self.transformer_tag, self.bounds_transformer = bounds_transformer
             try:
-                self._bounds_transformer.initialize(self._space)
-            except (AttributeError, TypeError):
-                raise TypeError('The transformer must be an instance of '
-                                'DomainTransformer')
+                self.bounds_transformer.initialize(self._space[self.transformer_tag])
+            except (AttributeError, TypeError) as e:
+                raise TypeError(
+                    "The transformer must be an instance of " "DomainTransformer"
+                )
 
         super().__init__(events=DEFAULT_EVENTS)
 
@@ -136,19 +152,23 @@ class BayesianOptimization(Observable):
         return self._space
 
     @property
-    def max(self):
-        return self._space.max()
+    def max(self, tag):
+        return self._space[tag].max()
 
     @property
-    def res(self):
-        return self._space.res()
+    def res(self, tag):
+        return self._space[tag].res()
 
-    def register(self, params, target):
+    def register(self, tag, params, target):
         """Expect observation with known target"""
-        self._space.register(params, target)
+        self._space[tag].register(params, target)
         self.dispatch(Events.OPTIMIZATION_STEP)
+        if self._bounds_transformer:
+            if self._transformer_tag == tag:
+                new_bounds = self._bounds_transformer.transform(self._space[tag])
+                self.set_bounds(new_bounds)
 
-    def probe(self, params, lazy=True):
+    def probe(self, tag, params, lazy=True):
         """
         Evaluates the function on the given points. Useful to guide the optimizer.
 
@@ -164,55 +184,57 @@ class BayesianOptimization(Observable):
         if lazy:
             self._queue.add(params)
         else:
-            self._space.probe(params)
+            self._space[tag].probe(params)
             self.dispatch(Events.OPTIMIZATION_STEP)
 
     def suggest(self, utility_function):
         """Most promising point to probe next"""
-        if len(self._space) == 0:
-            return self._space.array_to_params(self._space.random_sample())
+        space = self._space[self._tags[0]]
+        if len(space) == 0:
+            return space.array_to_params(space.random_sample())
 
         # Sklearn's GP throws a large number of warnings at times, but
         # we don't really need to see them here.
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            self._gp.fit(self._space.params, self._space.target)
+        # for tag in self._tags:
+        #     self._gp[tag].fit(self._space[tag].params, self._space[tag].target)
+        # with warnings.catch_warnings():
+        #     warnings.simplefilter("ignore")
+        #     for tag in self._tags:
+        #         self._gp[tag].fit(self._space[tag].params, self._space[tag].target)
+        for tag in self._tags:
+            self._gp[tag].fit(self._space[tag].params, self._space[tag].target)
 
         # Finding argmax of the acquisition function.
+        space = self._space[self._tags[0]]
         suggestion = acq_max(
-            ac=utility_function.utility,
+            ac=utility_function,
             gp=self._gp,
-            y_max=self._space.target.max(),
-            bounds=self._space.bounds,
-            random_state=self._random_state
+            bounds=space.bounds,
+            random_state=self._random_state,
         )
 
-        return self._space.array_to_params(suggestion)
+        return space.array_to_params(suggestion)
 
     def _prime_queue(self, init_points):
         """Make sure there's something in the queue at the very beginning."""
-        if self._queue.empty and self._space.empty:
+        space = self._space[self._tags[0]]
+        if self._queue.empty and space.empty:
             init_points = max(init_points, 1)
 
         for _ in range(init_points):
-            self._queue.add(self._space.random_sample())
+            self._queue.add(space.random_sample())
 
-    def _prime_subscriptions(self):
-        if not any([len(subs) for subs in self._events.values()]):
-            _logger = _get_default_logger(self._verbose)
-            self.subscribe(Events.OPTIMIZATION_START, _logger)
-            self.subscribe(Events.OPTIMIZATION_STEP, _logger)
-            self.subscribe(Events.OPTIMIZATION_END, _logger)
-
-    def maximize(self,
-                 init_points=5,
-                 n_iter=25,
-                 acq='ucb',
-                 kappa=2.576,
-                 kappa_decay=1,
-                 kappa_decay_delay=0,
-                 xi=0.0,
-                 **gp_params):
+    def maximize(
+        self,
+        init_points=5,
+        n_iter=25,
+        acq="ucb",
+        kappa=2.576,
+        kappa_decay=1,
+        kappa_decay_delay=0,
+        xi=0.0,
+        gp_params=None
+    ):
         """
         Probes the target space to find the parameters that yield the maximum
         value for the given function.
@@ -249,16 +271,23 @@ class BayesianOptimization(Observable):
         xi: float, optional(default=0.0)
             [unused]
         """
+        # TODO
+        raise NotImplementedError
         self._prime_subscriptions()
         self.dispatch(Events.OPTIMIZATION_START)
         self._prime_queue(init_points)
-        self.set_gp_params(**gp_params)
+        if gp_params is not None:
+            for tag, params in gp_params.items():
+                self.set_gp_params(tag, **params)
 
-        util = UtilityFunction(kind=acq,
-                               kappa=kappa,
-                               xi=xi,
-                               kappa_decay=kappa_decay,
-                               kappa_decay_delay=kappa_decay_delay)
+
+        util = UtilityFunction(
+            kind=acq,
+            kappa=kappa,
+            xi=xi,
+            kappa_decay=kappa_decay,
+            kappa_decay_delay=kappa_decay_delay,
+        )
         iteration = 0
         while not self._queue.empty or iteration < n_iter:
             try:
@@ -271,8 +300,7 @@ class BayesianOptimization(Observable):
             self.probe(x_probe, lazy=False)
 
             if self._bounds_transformer:
-                self.set_bounds(
-                    self._bounds_transformer.transform(self._space))
+                self.set_bounds(self._bounds_transformer.transform(self._space))
 
         self.dispatch(Events.OPTIMIZATION_END)
 
@@ -285,8 +313,11 @@ class BayesianOptimization(Observable):
         new_bounds : dict
             A dictionary with the parameter name and its new bounds
         """
-        self._space.set_bounds(new_bounds)
+        for tag in self._tags:
+            self._space[tag].set_bounds(new_bounds)
 
-    def set_gp_params(self, **params):
+    def set_gp_params(self, tag, **params):
         """Set parameters to the internal Gaussian Process Regressor"""
-        self._gp.set_params(**params)
+        self._gp[tag].set_params(**params)
+
+
